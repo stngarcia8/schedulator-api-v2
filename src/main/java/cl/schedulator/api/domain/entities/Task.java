f88ab1abf37e091ff1a75f0c2cd0fd7924e86c15
stngarcia <stngarcia8@gmail.com>
2022-02-21 06:56:20 -0300

    Adding anotations for task dto.

--- src/main/java/cl/schedulator/api/domain/entities/Task.java
+++ src/main/java/cl/schedulator/api/domain/entities/Task.java
@@ -1,10 +1,19 @@
 package cl.schedulator.api.domain.entities;
 
-import lombok.Builder;
-import lombok.Data;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
 @Builder
-@Data
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@ToString
 public class Task {
 
     private String taskId;
